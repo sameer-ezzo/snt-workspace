01cfde766566b5bc147848cb889d69b80eeff3df
Sameer <ezzo.sameer@gmail.com>
2022-12-18 17:41:41 +0300

    fixes

--- apps/web/src/app/client/bank-card/card.component.ts
+++ apps/web/src/app/client/bank-card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { AntiqueModel, AuctionModel } from 'libs/models/src';
 @Component({
   selector: 'snt-workspace-card',
