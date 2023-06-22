1d71657d7eab8a7b227aae3a3b3f1e6b1537029a
Sameer <ezzo.sameer@gmail.com>
2023-06-22 23:54:29 +0300

    updating and fixing styles

--- apps/api/src/app/users/users.service.ts
+++ apps/api/src/app/users/users.service.ts
@@ -8,15 +8,17 @@ export class UsersService {
 
 
     constructor(@InjectModel('User', 'SNT_DB') private userModel: Model<User>) { }
-    
+
     async findByEmail(email: string): Promise<User | undefined> {
         return this.userModel.findOne<User>({ email })
     }
 
     async create(payload: { password } & Partial<User>): Promise<any> {
 
-        const { email, password } = payload
+        const { password } = payload
+        const email = payload.email?.trim().toLocaleLowerCase()
         if (await this.findByEmail(email)) throw new Error('User already exists')
+
 
         if (!email || !password) throw new Error('Invalid payload')
         const user = { ...payload } as Partial<User>
